8db5fa942e15459445aaf79755fd737f3f1714dd
Cheer.L <1017801883@qq.com>
2017-04-26 15:46:06 +0800

    up

--- train.py
+++ train.py
@@ -6,8 +6,7 @@ import xgboost as xgb
 import pandas as pd
 
 
-LENGTH = 31
-NUM_ROUND = 100
+NUM_ROUND = 60
 LABEL_BOUND = 0.06
 
 
@@ -39,7 +38,7 @@ def xgboost_model(end_date, num_round=NUM_ROUND, num=1):
         'subsample': 0.8,
         'colsample_bytree': 0.8,
         'scale_pos_weight': 1,
-        'eta': 0.1,
+        'eta': 0.01,
         'silent': 1,
         'objective': 'binary:logistic',
         'eval_metric': 'logloss'
@@ -76,6 +75,7 @@ def xgboost_test(train_end_date, test_pred_end_date, bst=None, is_half=False):
         test_pred_end_date, is_cate8=True, is_half=is_half, is_odd=True)
     fact = test_pred_user_index.copy()
     fact['label'] = test_pred_label
+
     if bst is None:
         bst = xgboost_model(train_end_date)
     test_pred_result = bst.predict(xgb.DMatrix(test_pred_train_data.values))
@@ -86,8 +86,8 @@ def xgboost_test(train_end_date, test_pred_end_date, bst=None, is_half=False):
     pred['label'] = pred['label'].map(lambda x: 1 if x >= LABEL_BOUND else 0)
     pickle.dump(pred, open('cache/pred_%s.pkl' % (test_pred_end_date), 'wb'))
 
-    report(pred, fact)
-    # report(pred, pred_end_date=test_pred_end_date)
+    # report(pred, fact)
+    report(pred, pred_end_date=test_pred_end_date)
 
 
 if __name__ == '__main__':
